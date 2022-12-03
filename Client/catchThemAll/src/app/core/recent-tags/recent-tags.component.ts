d2897a1bbb2af38a7782e17d604fbfb74abbf86a
Matej Harabin <matejharabin@kosickaakademia.sk>
2022-12-03 20:10:16 +0100

    Marian pele

--- Client/catchThemAll/src/app/core/recent-tags/recent-tags.component.ts
+++ Client/catchThemAll/src/app/core/recent-tags/recent-tags.component.ts
@@ -11,18 +11,17 @@ export class RecentTagsComponent {
 
 
 
-  pythonTag: ITags = { name: "Python", count: "200" }
-  javaTag: ITags = { name: "Java", count: "20000" }
-  LuaTag: ITags = { name: "Lua", count: "1600" }
+  pythonTag: ITags = { name: "Python", count: 200 }
+  javaTag: ITags = { name: "Java", count: 20000 }
+  LuaTag: ITags = { name: "Lua", count: 1600 }
+
   allTags = [this.pythonTag, this.javaTag, this.LuaTag]
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  check() {
-
-  }
-
+  //if count in tag is over 1 000 it will change on 1k 
+  //another example 20 000 = 20k
 
 }
