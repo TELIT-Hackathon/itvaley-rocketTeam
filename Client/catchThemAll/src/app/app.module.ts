1acabb21444f15a3e7fabdc17fa233f4375c858a
Ladislav Krištofčo <ladislav.kristofco@kosickaakademia.sk>
2022-12-03 15:12:41 +0100

    Added home and nav-bar component

--- Client/catchThemAll/src/app/app.module.ts
+++ Client/catchThemAll/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
