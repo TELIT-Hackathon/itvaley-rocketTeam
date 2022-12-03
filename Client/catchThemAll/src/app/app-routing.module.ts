adcfa2b86a89beaf64b0233bad7003f1cb0f5c7f
Ladislav Krištofčo <ladislav.kristofco@kosickaakademia.sk>
2022-12-03 15:18:54 +0100

    Edit last commit

--- Client/catchThemAll/src/app/app-routing.module.ts
+++ Client/catchThemAll/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NavBarComponent} from "./core/nav-bar/nav-bar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'nav-bar', component: NavBarComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
