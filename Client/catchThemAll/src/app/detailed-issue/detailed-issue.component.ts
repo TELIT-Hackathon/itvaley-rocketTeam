8ac85ff9636d6d0daf9c77c20d2db85c2edd9f0e
Matej Harabin <matejharabin@kosickaakademia.sk>
2022-12-03 22:00:09 +0100

    Pls laďo fixni to , ja to už nedavam

---
+++ Client/catchThemAll/src/app/detailed-issue/detailed-issue.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-issue',
+  templateUrl: './detailed-issue.component.html',
+  styleUrls: ['./detailed-issue.component.scss']
+})
+export class DetailedIssueComponent {
+
+}
