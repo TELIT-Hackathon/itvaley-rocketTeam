0f19c87f545a0c2ea1aa7f876fefb9ca2da926ea
Matej Harabin <matejharabin@kosickaakademia.sk>
2022-12-03 23:32:27 +0100

    Added 404 route

--- Client/catchThemAll/src/app/app-routing.module.ts
+++ Client/catchThemAll/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-import {NavBarComponent} from "./core/nav-bar/nav-bar.component";
-import {ProfileNavComponent} from "./core/profile-nav/profile-nav.component";
-import {LoginComponent} from "./account/login/login.component";
-import {RegisterComponent} from "./account/register/register.component";
+import { HomeComponent } from "./home/home.component";
+import { NavBarComponent } from "./core/nav-bar/nav-bar.component";
+import { ProfileNavComponent } from "./core/profile-nav/profile-nav.component";
+import { LoginComponent } from "./account/login/login.component";
+import { RegisterComponent } from "./account/register/register.component";
+import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'nav-bar', component: NavBarComponent },
-  { path: 'profile-nav', component: ProfileNavComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'profile-nav', component: ProfileNavComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' }
 
 ];
 
