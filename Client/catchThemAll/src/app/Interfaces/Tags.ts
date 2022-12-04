73f2401705cdfb0096a9aa898cc5e679fea25aeb
Matej Harabin <matejharabin@kosickaakademia.sk>
2022-12-04 03:45:58 +0100

    current task fixed

--- Client/catchThemAll/src/app/Interfaces/Tags.ts
+++ Client/catchThemAll/src/app/Interfaces/Tags.ts
@@ -2,3 +2,14 @@ export interface ITags {
     name: string,
     count: number | string
 }
+
+export class Tags {
+    static clone(t: Tags): Tags {
+        return new Tags(t.name, t.count)
+    }
+
+    constructor(
+        public name: string,
+        public count: number,
+    ) { }
+}
