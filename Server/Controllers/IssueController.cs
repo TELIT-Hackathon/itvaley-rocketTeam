c4392a60f8951f35d4ed28d6764a895722a6c001
penkys <88934551+penkys@users.noreply.github.com>
2022-12-04 08:54:54 +0100

    ID and Name find

--- Server/Controllers/IssueController.cs
+++ Server/Controllers/IssueController.cs
@@ -85,21 +85,53 @@ public class IssueController : ControllerBase
     }
     
     [HttpGet(Name = "getIssuesByIssueId")]
-    public async Task<List<Issue>> GetAllIssueByIssueId(int issueId)
+    public async Task<List<DbIssueDto>> GetAllIssueByIssueId(int issueId)
     {
         var repository = new IssueRepository(DatabaseContext.Instance);
+        var relationRepository = new IssueTagRelationRepository(DatabaseContext.Instance);
+        var tagsRepository = new TagsRepository(DatabaseContext.Instance);
         var issues = await repository.GetIssues();
-        var issuesById = new List<Issue>();
+        var dtoIssues = new List<DbIssueDto>();
         
-        issues.ForEach(iss =>
+        issues.ForEach(issue =>
         {
-            if (iss.IssueId == issueId)
+            var tagRelations = relationRepository.GetTags(issue.IssueId);
+            var tagNameDtos = new List<TagNameDto>();
+            if (tagRelations != null)
             {
-                issuesById.Add(iss);
+                foreach (var tagRelation in tagRelations)
+                {
+                    tagNameDtos.Add(new TagNameDto()
+                    {
+                        Name = tagsRepository.GetTagById(tagRelation.TagId).Result.Name
+                    });
+                }
             }
+                
+            var dbIssueDto = new DbIssueDto()
+            {
+                Date = issue.Date,
+                Tags = tagNameDtos,
+                Text = issue.Text,
+                Title = issue.Title,
+                IsSolved = issue.IsSolved,
+                IssueId = issue.IssueId,
+                Username = issue.Username
+            };
+            dtoIssues.Add(dbIssueDto);
         });
+
+        var issuesByIssueId = new List<DbIssueDto>();
+        foreach (var dbIssueDto in dtoIssues)
+        {
+            if (dbIssueDto.IssueId == issueId)
+            {
+                issuesByIssueId.Add(dbIssueDto) ;
+            }
+        }
         
-        return issuesById ;
+        
+        return issuesByIssueId ;
     }
     
     [HttpGet(Name = "getIssuesByTags")]
@@ -110,19 +142,51 @@ public class IssueController : ControllerBase
     }
     
     [HttpGet(Name = "getIssuesByName")]
-    public async Task<List<Issue>> GetAllIssueByName(string issueName)
+    public async Task<List<DbIssueDto>> GetAllIssueByName(string issueName)
     {
         var repository = new IssueRepository(DatabaseContext.Instance);
+        var relationRepository = new IssueTagRelationRepository(DatabaseContext.Instance);
+        var tagsRepository = new TagsRepository(DatabaseContext.Instance);
         var issues = await repository.GetIssues();
-        var issuesByName = new List<Issue>();
+        var dtoIssues = new List<DbIssueDto>();
         
-        issues.ForEach(iss =>
+        issues.ForEach(issue =>
         {
-            if (iss.Title == issueName)
+            var tagRelations = relationRepository.GetTags(issue.IssueId);
+            var tagNameDtos = new List<TagNameDto>();
+            if (tagRelations != null)
             {
-                issuesByName.Add(iss);
+                foreach (var tagRelation in tagRelations)
+                {
+                    tagNameDtos.Add(new TagNameDto()
+                    {
+                        Name = tagsRepository.GetTagById(tagRelation.TagId).Result.Name
+                    });
+                }
             }
+                
+            var dbIssueDto = new DbIssueDto()
+            {
+                Date = issue.Date,
+                Tags = tagNameDtos,
+                Text = issue.Text,
+                Title = issue.Title,
+                IsSolved = issue.IsSolved,
+                IssueId = issue.IssueId,
+                Username = issue.Username
+            };
+            dtoIssues.Add(dbIssueDto);
         });
+
+        var issuesByName = new List<DbIssueDto>();
+        foreach (var dbIssueDto in dtoIssues)
+        {
+            if (dbIssueDto.Title == issueName)
+            {
+                issuesByName.Add(dbIssueDto) ;
+            }
+        }
+        
         
         return issuesByName ;
     }
